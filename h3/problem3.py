e659bc85960ed4c755485b61397ca25e3178eab3
Matheus C. Fernandes <matheuscfernandes@gmail.com>
2017-03-23 14:56:41 -0400

    finished a lot of the code for problem 3, updated the tex files

--- h3/problem3.py
+++ h3/problem3.py
@@ -8,35 +8,86 @@ import random
 
 # Implement this class
 class KernelPerceptron(Perceptron):
-	def __init__(self, numsamples):
-		self.numsamples = numsamples
+    def __init__(self, numsamples):
+        self.numsamples = numsamples
 
-	# Implement this!
-	def fit(self, X, Y):
-		S={};b=0
-		
-		
-		# for
+    def __Kernel1(self,X,XP):
+        return np.dot(X,XP)
 
-	self.w=1
-	# Implement this!
-	def predict(self, X):
+    # Implement this!
+    def fit(self, X, Y):
+        numSamples=self.numsamples
+        numOfPoints=len(X)
+        self.X=X
+        self.Y=Y
 
-		classY=
+        S=[]; alpha={x:0 for x in xrange(numOfPoints)}
+
+        for num in xrange(numSamples):
+            t=random.randint(0,numOfPoints-1)
+            Xt=X[t];Yt=Y[t]
+            YHatT=0
+
+            for i in S:
+                YHatT+=alpha[i]*self.__Kernel1(Xt,X[i])
+
+            if Yt*YHatT <=0:
+                S.append(t)
+                alpha[t]=Yt
+
+        w=np.zeros((len(X[i]),0))
+
+        for i in xrange(numOfPoints):
+            for j in xrange(len(X[i])):
+                w[j]+=alpha[i]*X[i,j]
+
+        self.w=w
+    # Implement this!
+    def predict(self, X):
+        return np.dot(np.transpose(self.w),np.transpose(X))
+
 
 # Implement this class
 class BudgetKernelPerceptron(Perceptron):
-	def __init__(self, beta, N, numsamples):
-		self.beta = beta
-		self.N = N
-		self.numsamples = numsamples
-		
-	# Implement this!
-	# def fit(self, X, Y):
+    def __init__(self, beta, N, numsamples):
+        self.beta = beta
+        self.N = N
+        self.numsamples = numsamples
 
-	# Implement this!
-	# def predict(self, X):
+    # Implement this!
+    def fit(self, X, Y):
+        numSamples=self.numsamples
+        beta=self.beta
+        N=self.N
+        self.X=X
+        self.Y=Y
 
+        numOfPoints=len(X)
+
+        S=[]; alpha={x:0 for x in xrange(numOfPoints)}
+
+        for num in xrange(numSamples):
+            t=random.randint(0,numOfPoints-1)
+            Xt=X[t];Yt=Y[t]
+            YHatT=0
+
+            for i in S:
+                YHatT+=alpha[i]*self.__Kernel1(Xt,X[i])
+
+            if Yt*YHatT <=beta:
+                S.append(t)
+                alpha[t]=Yt
+                if abs(S)>N:
+                    S=0
+
+        for i in xrange(numOfPoints):
+            for j in xrange(len(X[i])):
+                w[j] += alpha[i] * X[i, j]
+
+        self.w = w
+    # Implement this!
+    def predict(self, X):
+        return np.dot(np.transpose(self.w),np.transpose(X))
 
 
 # Do not change these three lines.
